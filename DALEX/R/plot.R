c667c094a1d8f7946cb13d232141951ae4df1675
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2018-02-13 01:05:37 +0800

    less parameters in the plot function

--- DALEX/R/plot.R
+++ DALEX/R/plot.R
@@ -1,16 +1,16 @@
-#' Title
+#' Plots Marginal Responses
 #'
-#' @param response1
-#' @param ...
-#' @param color
-#' @param shape
+#' Function 'plot.single_variable_explainer' plots marginal responses for one or more explainers.
+#'
+#' @param response1 a single variable exlainer produced with the 'single_variable' function
+#' @param ... other explainers that shall be plotted together
 #'
 #' @return
 #' @export
 #'
 #' @examples
 #'
-plot.single_variable_explainer <- function(response1, ..., color = "label", shape = "type") {
+plot.single_variable_explainer <- function(response1, ...) {
   df <- response1
   class(df) <- "data.frame"
 
@@ -23,9 +23,13 @@ plot.single_variable_explainer <- function(response1, ..., color = "label", shap
   }
 
   variable_name <- head(df$var, 1)
-  ggplot(df, aes(x, y), aes_string(color = color, shape = shape)) +
+  ggplot(df, aes_string(x = "x", y = "y", color = "label", shape = "type")) +
     geom_point() +
     geom_line() +
+    theme_mi2() +
+    scale_color_brewer(name = "Model", type = "qual", palette = "Dark2") +
+    scale_shape_discrete(name = "Type") +
+    ggtitle("Single variable conditional responses") +
     xlab(variable_name) + ylab(expression(hat("y")))
 
 }
