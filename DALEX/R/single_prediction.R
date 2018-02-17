c6de2e1045011047ea1ccade0496fbdc9c2f53cf
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2018-02-18 01:34:07 +0800

    support for single predictions via breakDown

---
+++ DALEX/R/single_prediction.R
@@ -0,0 +1,29 @@
+#' Explanations for a Single Prediction
+#'
+#'
+#'
+#' @param explainer a model to be explained, preprocessed by the 'explain' function
+#' @param observation a
+#' @param ...
+#'
+#' @return An object of the class 'single_prediction_explainer'.
+#' It's a data frame with calculated average response.
+#'
+#' @export
+#' @import breakDown
+#' @import live
+#' @examples
+#'
+single_prediction <- function(explainer, observation, ...) {
+  stopifnot(class(explainer) == "explainer")
+
+  # breakDown
+  res <- broken(explainer$model, new_observation = observation, baseline = "Intercept")
+  res$label <- rep(explainer$label, length(res$variable))
+
+  class(res) <- c("single_prediction_explainer", "data.frame")
+  res
+
+}
+
+
