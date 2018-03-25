7132268feb8dc43d024b5c0ddbc35cb77048551e
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2018-03-26 01:24:59 +0200

    czyszczenie struktury

--- DALEX/R/zzz.R
+++
@@ -1,8 +0,0 @@
-.onAttach <- function(...) {
-  packageStartupMessage("Welcome to DALEX (version: ", utils::packageVersion("DALEX"), ").")
-}
-
-## no S4 methodology here; speedup :
-.noGenerics <- TRUE
-
-
--- __examples__.R
+++ __examples__.R
@@ -70,8 +70,72 @@ variable = "satisfaction_level"
 
 # ------
 
+# variable importance
+unloadNamespace("DALEX")
+library("DALEX")
+library("randomForest")
+library("xgboost")
+library("breakDown")
+
+HR_rf_model <- randomForest(left~., data = breakDown::HR_data, ntree = 100)
+explainer_rf  <- explain(HR_rf_model, data = HR_data, y = HR_data$left)
+
+variable_dropout(explainer_rf)
+
+HR_glm_model <- glm(left~., data = breakDown::HR_data, family = "binomial")
+model_martix_train <- model.matrix(left~.-1, breakDown::HR_data)
+data_train <- xgb.DMatrix(model_martix_train, label = breakDown::HR_data$left)
+param <- list(max_depth = 2, eta = 1, silent = 1, nthread = 2,
+              objective = "binary:logistic", eval_metric = "auc")
+HR_xgb_model <- xgb.train(param, data_train, nrounds = 50)
+
+explainer_rf  <- explain(HR_rf_model, data = HR_data, y = HR_data$left)
+explainer_glm <- explain(HR_glm_model, data = HR_data, y = HR_data$left)
+explainer_xgb <- explain(HR_xgb_model, data = model_martix_train, y = HR_data$left, label = "xgboost")
 
 
+logit <- function(x) exp(x)/(1+exp(x))
+
+vd1 <- variable_dropout(explainer_rf, type = "raw")
+vd2 <- variable_dropout(explainer_glm, type = "raw",
+                 loss_function = function(observed, predicted) sum((observed - logit(predicted))^2))
+vd3 <- variable_dropout(explainer_xgb, type = "raw")
+
+plot(vd1)
+plot(vd2)
+plot(vd3)
+
+plot(vd1, vd2, vd3)
+
+nn <- rbind(vd1, vd2, vd3)
+
+library(ggplot2)
+
+ggplot(nn, aes(label, dropout_loss, shape = variable, group=variable)) +
+  geom_point(position = "dodge") +
+  geom_line() + scale_shape_manual(values=LETTERS) +
+  coord_flip()
+
+library(dplyr)
+expl_df <- nn
+bestFits <- expl_df[expl_df$variable == "_full_model_", ]
+ext_expl_df <- merge(expl_df, bestFits[,c("label", "dropout_loss")], by = "label")
+
+ext_expl_df$variable <- reorder(ext_expl_df$variable,
+                                ext_expl_df$dropout_loss.x - ext_expl_df$dropout_loss.y,
+                                mean)
+max_vars = 10
+trimmed_parts <- lapply(unique(ext_expl_df$label), function(label) {
+  tmp <- ext_expl_df[ext_expl_df$label == label, ]
+  tmp[tail(order(tmp$dropout_loss.x), max_vars), ]
+})
+ext_expl_df <- do.call(rbind, trimmed_parts)
+
+ggplot(ext_expl_df, aes(variable, ymin = dropout_loss.y, ymax = dropout_loss.x)) +
+  geom_errorbar() + coord_flip() +
+  facet_wrap(~label, ncol = 1, scales = "free_y") +
+  ylab("Drop-out Loss") + xlab("") +
+  theme_mi2()
 
 
 
@@ -82,4 +146,38 @@ yhat <- function(X.model, newdata) as.numeric(predict(X.model, newdata))
 res <- ALEPlot(breakDown::HR_data, HR_rf_model, K=40, yhat, J = variable)
 res <- ALEPlot(breakDown::HR_data, HR_rf_model, K=50, yhat, J = variable)
 res <- ALEPlot(breakDown::HR_data, HR_rf_model, K=100, yhat, J = variable)
+
+
+# ----------
+
+library(gbm)
+library(DALEX)
+library(breakDown)
+
+# create a gbm model
+model <- gbm(quality ~ pH + residual.sugar + sulphates + alcohol, data = wine,
+             distribution = "gaussian",
+             n.trees = 1000,
+             interaction.depth = 4,
+             shrinkage = 0.01,
+             n.minobsinnode = 10,
+             verbose = FALSE)
+
+# make an explainer for the model
+explainer_gbm <- explain(model, data = wine)
+
+# create a new observation
+new.wine <- data.frame(citric.acid = 0.35,
+                       sulphates = 0.6,
+                       alcohol = 12.5,
+                       pH = 3.36,
+                       residual.sugar = 4.8)
+
+exp_sgn <- single_prediction(explainer_gbm, observation = new.wine, n.trees = 1000)
+
+exp_sgn
+plot(exp_sgn)
+
+
+
 
