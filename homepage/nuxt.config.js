5d4c7ba48998f44c84b3ad40b2aa93da6de9a034
Piotr Piątyszek <piotrp@wektor.xyz>
2020-12-24 01:03:51 +0100

    Fix nuxt config

--- homepage/nuxt.config.js
+++ homepage/nuxt.config.js
@@ -17,7 +17,7 @@ export default {
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
-    '@fortawesome/fontawesome/styles.css',
+    '@fortawesome/fontawesome/styles.css'
   ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
